9f5832601bc45c3aac68d26a2f6ad0a9694a9a40
webestudiorocha <web@estudiorochayasoc.com.ar>
2019-01-09 12:30:05 -0300

    fuighdugiof

--- assets/inc/header.inc.php
+++ assets/inc/header.inc.php
@@ -1,6 +1,15 @@
 <title>Arsol</title>
 
 <meta charset="utf-8">
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-127300251-17"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-127300251-17');
+</script>
 
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">

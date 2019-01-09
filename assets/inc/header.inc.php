da8782ff8a5d0418c27fdb42a06c4d7e03847d63
webestudiorocha <web@estudiorochayasoc.com.ar>
2019-01-09 12:33:27 -0300

    fgg

--- assets/inc/header.inc.php
+++ assets/inc/header.inc.php
@@ -13,7 +13,17 @@
 
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-
+<!-- Begin Inspectlet Asynchronous Code -->
+<script type="text/javascript">
+    (function() {
+        window.__insp = window.__insp || [];
+        __insp.push(['wid', 193188888]);
+        var ldinsp = function(){
+            if(typeof window.__inspld != "undefined") return; window.__inspld = 1; var insp = document.createElement('script'); insp.type = 'text/javascript'; insp.async = true; insp.id = "inspsync"; insp.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://cdn.inspectlet.com/inspectlet.js?wid=193188888&r=' + Math.floor(new Date().getTime()/3600000); var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(insp, x); };
+        setTimeout(ldinsp, 0);
+    })();
+</script>
+<!-- End Inspectlet Asynchronous Code -->
 <link href='http://fonts.googleapis.com/css?family=Noto+Sans:400,700' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" href="<?= URL; ?>/assets/css/bootstrap.min.css" type="text/css" media="screen">
 <link rel="stylesheet" href="<?= URL; ?>/assets/css/jquery.bxslider.css" type="text/css" media="screen">
